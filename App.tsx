b84fd8435cb9eb1bd78be5206645866bbffbe1e7
3lPr1mo <erickenriquechaparro@gmail.com>
2023-10-27 12:44:28 -0500

    Register schema completed

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/auth/login/ui/LoginScreen';
+import SignInScreen from './src/auth/signIn/ui/SingInScreen';
 
 export default function App() {
   return (
@@ -12,6 +13,8 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    <LoginScreen />
+    <SignInScreen />
  */
 const styles = StyleSheet.create({
   container: {

f79338b7bb732a8075eda672e93d185defbab0fd
SnakyDH <skdanielg5@gmail.com>
2023-10-29 20:10:52 -0500

    feat: history screen

--- src/history/ui/HistoryScreen.tsx
+++ src/history/ui/HistoryScreen.tsx
@@ -1,9 +1,58 @@
-import { Text, View } from 'react-native';
-
+import { View, Pressable } from 'react-native';
+import { Colors } from '../../Constants/Colors';
+import RecordButton from '../../components/RecordButton';
+// todo: organize in list
 export default function HistoryScreen() {
   return (
-    <View>
-      <Text>HistoryScreen</Text>
+    <View style={{ backgroundColor: Colors.backgroundPage, height: '100%' }}>
+      <Pressable>
+        <RecordButton
+          emotionPrimary='Ira'
+          emotionSecondary='Celos'
+          color={Colors.red}
+          backgroundColor={Colors.redDark}
+        />
+      </Pressable>
+      <Pressable>
+        <RecordButton
+          emotionPrimary='Tristeza'
+          emotionSecondary='Soledad'
+          color={Colors.blue}
+          backgroundColor={Colors.blueDark2}
+        />
+      </Pressable>
+      <Pressable>
+        <RecordButton
+          emotionPrimary='Felicidad'
+          emotionSecondary='Poder'
+          color={Colors.yellowDark}
+          backgroundColor={Colors.brownDark}
+        />
+      </Pressable>
+      <Pressable>
+        <RecordButton
+          emotionPrimary='Sorpresa'
+          emotionSecondary='Efusivo'
+          color={Colors.purple}
+          backgroundColor={Colors.prupleDark}
+        />
+      </Pressable>
+      <Pressable>
+        <RecordButton
+          emotionPrimary='Asco'
+          emotionSecondary='Desprecio'
+          color={Colors.green}
+          backgroundColor={Colors.greenDark}
+        />
+      </Pressable>
+      <Pressable>
+        <RecordButton
+          emotionPrimary='Felicidad'
+          emotionSecondary='Poder'
+          color={Colors.yellowDark}
+          backgroundColor={Colors.brownDark}
+        />
+      </Pressable>
     </View>
   );
 }

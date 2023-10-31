4218c7c236b350c71b7c11934425f48b46d13f6e
Erick Chaparro <erickenriquechaparro@gmail.com>
2023-10-31 11:49:43 -0500

    Schemas completed

--- src/home/ui/HomeScreen.tsx
+++ src/home/ui/HomeScreen.tsx
@@ -1,11 +1,47 @@
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet, Pressable } from 'react-native';
+import DiaryButton from './components/DiaryButton';
+import { Colors } from '../../Constants/Colors';
 
 export default function HomeScreen() {
   return (
-    <View style={{ position: 'absolute', bottom: 500, right: 100, left: 100 }}>
-      <View>
-        <Image source={require('../../../assets/bee.png')} style={styles.image}/>
+    <View
+      style={{
+        backgroundColor: Colors.backgroundPage,
+        height: '100%',
+        alignItems: 'center',
+      }}
+    >
+      <View
+        style={{
+          top: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginBottom: 30,
+        }}
+      >
+        <Image
+          source={require('../../../assets/bee.png')}
+          style={styles.image}
+        />
         <Text style={styles.title}>Registro emocional</Text>
+      </View>
+      <View style={{margin: 20}}>
+        <Pressable>
+          <DiaryButton
+            backgroundColor={Colors.yellow}
+            outlining={Colors.red}
+            title='Emoción Trascendental'
+            image='2'
+          />
+        </Pressable>
+        <Pressable>
+          <DiaryButton
+            backgroundColor={Colors.yellow}
+            outlining={Colors.redDark}
+            title='Diario Emocional'
+            image='1'
+          />
+        </Pressable>
       </View>
     </View>
   );
@@ -13,12 +49,12 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   image: {
-    width: 150,
-    height: 150,
+    width: 130,
+    height: 130,
     marginBottom: 10,
   },
   title: {
-    fontSize: 50,
+    fontSize: 40,
     fontWeight: 'bold',
     color: '#FFF500',
     marginBottom: 10,

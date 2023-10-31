698533ef38db72ae9a6c54e2bc3081c00a2b8b65
Erick Chaparro <erickenriquechaparro@gmail.com>
2023-10-30 23:14:41 -0500

    Image and Title on HomeScreen init

--- src/home/ui/HomeScreen.tsx
+++ src/home/ui/HomeScreen.tsx
@@ -1,9 +1,26 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={{ position: 'absolute', bottom: 500, right: 100, left: 100 }}>
-      <Text>HomeScrenn</Text>
+      <View>
+        <Image source={require('../../../assets/bee.png')} style={styles.image}/>
+        <Text style={styles.title}>Registro emocional</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#FFF500',
+    marginBottom: 10,
+  },
+});

e708d6895b3816b02bfaef854a81c44375dd06d0
Erick Chaparro <erickenriquechaparro@gmail.com>
2023-11-20 11:07:28 -0500

    Secondary Emotions Enum completed

--- src/Constants/Emotions.ts
+++ src/Constants/Emotions.ts
@@ -6,3 +6,39 @@ export enum Emotions {
   SURPRISE = 'Sorpresa',
   DISGUST = 'Asco',
 }
+
+export enum Happy{
+  GRATITUDE = 'Gratitud',
+  SATISFACTION = 'Satisfacción',
+  EUPHORIA = 'Euforia',
+}
+
+export enum Sad{
+  MELANCHOLY = 'Melancolía',
+  LONELY = 'Soledad',
+  DISCOURAGEMENT = 'Desánimo',
+}
+
+export enum Angry{
+  HATE = 'Odio',
+  RAGE = 'Rabia',
+  ANNOYANCE = 'Molestia',
+}
+
+export enum Fear{
+  TERROR = 'Terror',
+  NERVOUS = 'Nerviosismo',
+  WORRY = 'Preocupación',
+}
+
+export enum Surprise{
+  PERPLEXITY = 'Perplejidad',
+  CURIOSITY = 'Curiosidad',
+  CONFUSION = 'Confusión',
+}
+
+export enum Disgust{
+  REJECTION = 'Rechazo',
+  DISAPPROVAL = 'Desaprobación',
+  DISCOMFORT = 'Incomodidad',
+}
